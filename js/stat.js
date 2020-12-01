99abf074237409f06a39de6340f19079504cdd12
and-adad <Andreyadad@gmail.com>
2020-12-01 23:45:05 +0500

    Исправил алгоритм, он теперь работает на любом количестве игроков

--- js/stat.js
+++ js/stat.js
@@ -2,24 +2,31 @@
 
 var CLOUD_WIDTH = 500;
 var CLOUD_HEIGHT = 200;
+var CLOUD_X = 100;
+var CLOUD_Y = 50;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH =50;
+var BAR_HEIGHT = 20;
+var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 
-var renderCloud = function (ctx, x, y, color) {
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+var renderCloud = function(ctx, x, y, color) {
+    ctx.fillStyle =color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-window.renderStatistics = function(ctx) {
-	renderCloud(ctx, 110, 60, 'rgba(0, 0, 0, 0.3)');
-	renderCloud(ctx, 100, 50, '#fff');
+};
 
-	ctx.fillStyle = '#000';
-	ctx.fillText('Вы', 110, 75);
-	ctx.fillRect(110, 60, 480, 20);
+window.renderStatistics = function (ctx) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, 'fff');
 
-	ctx.fillText('Иван', 110, 105);
-	ctx.fillRect(160, 90, 430, 20);
+    ctx.fillStyle = '#000';
 
-	ctx.fillText('Юлия', 110, 135);
-	ctx.fillRect(160, 120, 430, 20);
+    var players = ['Вы', 'Иван', 'Юлия', 'Кекс'];
 
+    for (var i = 0; i < players.length; i++) {
+        ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP +(GAP + BAR_HEIGHT) * i);
+        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP +(GAP
+         + BAR_HEIGHT) * i, barWidth, BAR_HEIGHT);
+    }
 };
