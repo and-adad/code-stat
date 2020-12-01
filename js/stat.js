be130f012fdfa481af5f6e52d8edb69bb63e9431
and-adad <Andreyadad@gmail.com>
2020-12-02 00:43:22 +0500

    Исправил цвет облака на белый

--- js/stat.js
+++ js/stat.js
@@ -31,7 +31,7 @@ var getMaxElement = function(arr) {
 
 window.renderStatistics = function (ctx, players, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
-    renderCloud(ctx, CLOUD_X, CLOUD_Y, 'fff');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
     ctx.fillStyle = '#000';
 
