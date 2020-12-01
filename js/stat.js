6a53bb26a00c988bbfff720efd561a67b8a417bd
and-adad <Andreyadad@gmail.com>
2020-12-01 23:48:56 +0500

    Исправил размер каждого столбца гистограммы пропорционально заработанным игроком очкам

--- js/stat.js
+++ js/stat.js
@@ -11,22 +11,34 @@ var BAR_HEIGHT = 20;
 var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
 
 var renderCloud = function(ctx, x, y, color) {
+
     ctx.fillStyle =color;
     ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
 };
 
-window.renderStatistics = function (ctx) {
+var getMaxElement = function(arr) {
+    var maxElement = arr[0];
+
+    for (var  i = 0; i < arr.length; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    return maxElement;
+}
+
+window.renderStatistics = function (ctx, players, times) {
     renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
     renderCloud(ctx, CLOUD_X, CLOUD_Y, 'fff');
 
     ctx.fillStyle = '#000';
 
-    var players = ['Вы', 'Иван', 'Юлия', 'Кекс'];
+    var maxTime = getMaxElement(times);
 
     for (var i = 0; i < players.length; i++) {
-        ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP +(GAP + BAR_HEIGHT) * i);
-        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP +(GAP
-         + BAR_HEIGHT) * i, barWidth, BAR_HEIGHT);
+        ctx.fillText(players[i],CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP +(GAP + BAR_HEIGHT) * i);
+        ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP +(GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
     }
 };
